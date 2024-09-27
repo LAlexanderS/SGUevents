aeac66ed61bbe5fc2908d30b97d5696ed27e5e6b
LAlexanderS <likov.shura2003@mail.ru>
2024-09-27 17:55:17 +0700

    arrow-page

--- users/forms.py
+++ users/forms.py
@@ -6,7 +6,7 @@ class RegistrationForm(forms.ModelForm):
         max_length=50,
         required=True,
         label='ID отдела',
-        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Введите ID отдела'})
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': '1-1-1'})
     )
 
     class Meta:
